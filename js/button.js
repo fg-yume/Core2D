a7692036d247ef46ec7e53da28cc98ca73066e19
Freddy Garcia <fsg7957@rit.edu>
2014-06-07 16:03:15 -0400

    FoodFriends' button update - Fixed errors where certain button settings could not be set to default - added image support

--- js/button.js
+++ js/button.js
@@ -23,11 +23,67 @@ Core2D.Button = (function()
 	 * Creates a button based on the given properties
 	 * TODO: pass in a label as part of the button
 	 *
-	 * @param	{Object} properties     the properties that will be given to the button
+	 * @param   {Object} properties     the properties that will be given to the button
 	 *
 	 * @return  none
 	 */
 	var Button = function(properties)
+	{
+		this.modify(properties);
+		
+		this.currentState  = Core2D.BUTTON_STATE.MAIN;
+		this.previousState = Core2D.BUTTON_STATE.MAIN;
+	};
+	
+	/*
+	 * Changes the state of the button
+	 *
+	 * @param   the state to change into
+	 *
+	 * @return  none
+	 */
+	Button.prototype.changeState = function(newState)
+	{
+	    this.previousState = this.currentState;
+		this.currentState = newState;
+		
+	    // transition action to new state
+	    switch(this.currentState)
+	    {
+	    case Core2D.BUTTON_STATE.CLICK:
+		    this.callbacks.onClick();
+			break;
+			
+		case Core2D.BUTTON_STATE.HOVER:
+		    this.callbacks.onHover();
+			break;
+			
+		default:
+		    this.callbacks.onMain();
+			break;
+	    };
+	};
+	
+	Button.prototype.getState = function()
+	{
+		return this.currentState;
+	};
+	
+	/*
+	 * Updates the button
+	 *
+	 * @return	none
+	 */
+	Button.prototype.update = function()
+	{
+	};
+	
+	/*
+	 * Modifies the button based on properties passed in
+	 *
+	 * @return  none
+	 */
+	Button.prototype.modify = function(properties)
 	{
 		// covering bases
 		var properties       = properties           || {};
@@ -38,16 +94,15 @@ Core2D.Button = (function()
 		properties.text      = properties.text      || {};
 		properties.callbacks = properties.callbacks || {};
 	
-		// Settings -----------------------------------------------------
 		this.center = {
-			x : properties.center.x === undefined ? window.innerWidth / 2 
-			    : properties.center.x,
-			y : properties.center.y === undefined ? window.innerHeight / 2
-			    : properties.center.y
+		    x : properties.center.x === undefined ? window.innerWidth / 2 
+		        : properties.center.x,
+		    y : properties.center.y === undefined ? window.innerHeight / 2
+		        : properties.center.y
 		};
 		
 		this.size	= {
-			width  : properties.size.width  === undefined ? 30
+		    width  : properties.size.width  === undefined ? 30
 			    : properties.size.width,
 			height : properties.size.height === undefined ? 10
 			    : properties.size.height,
@@ -55,22 +110,29 @@ Core2D.Button = (function()
 			    : properties.size.stroke
 		};
 		
-		this.color  = {
-			fill   : properties.color.fill   === undefined ? "#4C6B88"
-			    : properties.color.fill,
-			stroke : properties.color.stroke === undefined ? "#4C6B88"
-			    : properties.color.stroke
-		};
+		// image or color
+		if(properties.image)
+			this.image = properties.image; // requires image to already be loaded!
 		
-		this.hover  = {
-			fill   : properties.hover.fill   === undefined ? this.color.fill
-			    : properties.hover.fill,
-			stroke : properties.hover.stroke === undefined ? this.color.stroke
-			    : properties.hover.stroke
-		};
+		else
+		{
+			this.color  = {
+				fill   : properties.color.fill   === undefined ? "#4C6B88"
+					: properties.color.fill,
+				stroke : properties.color.stroke === undefined ? "#4C6B88"
+					: properties.color.stroke
+			};
+			
+			this.hover  = {
+				fill   : properties.hover.fill   === undefined ? this.color.fill
+					: properties.hover.fill,
+				stroke : properties.hover.stroke === undefined ? this.color.stroke
+					: properties.hover.stroke
+			};
+		}
 		
 		this.text   = {
-			string    : properties.text.string === undefined ? "Default"
+			string    : properties.text.string === undefined ? ""
 			    : properties.text.string,
 			color     : properties.text.color  === undefined ? "#EFECDE"
 			    : properties.text.color,
@@ -83,28 +145,13 @@ Core2D.Button = (function()
 		};
 		
 		this.callbacks = {
-			hover  : properties.callbacks.hover === undefined ? function(){console.log("default hover");}
+			onHover  : properties.callbacks.hover === undefined ? function(){console.log("default hover");}
 			    : properties.callbacks.hover,
-			click  : properties.callbacks.click === undefined ? function(){console.log("default click");}
+			onClick  : properties.callbacks.click === undefined ? function(){console.log("default click");}
 			    : properties.callbacks.click,
-			main   : properties.callbacks.main  === undefined ? function(){console.log("default main");}
+			onMain   : properties.callbacks.main  === undefined ? function(){console.log("default main");}
 			    : properties.callbacks.main
 		};
-		
-		this.currentState = Core2D.BUTTON_STATE.MAIN;
-	};
-	
-	/*
-	 * Updates the button
-	 *
-	 * @return	none
-	 */
-	Button.prototype.update = function()
-	{
-		//if(this.clicked /*&& "function" == typeof func*/)
-			//this.func();
-			
-		//this.clicked = false;*/
 	};
 	
 	/*
@@ -122,43 +169,73 @@ Core2D.Button = (function()
 			switch(this.currentState)
 			{
 			case Core2D.BUTTON_STATE.MAIN:
-				ctx.fillStyle   = this.color.fill;
-				ctx.strokeStyle = this.color.stroke;
+				if(this.image)
+				{
+				    // no need to set styles
+				}
+				
+				else
+				{
+				    ctx.fillStyle   = this.color.fill;
+				    ctx.strokeStyle = this.color.stroke;
+				}
 				
 				break;
 				
 			case Core2D.BUTTON_STATE.HOVER:
-				ctx.fillStyle   = this.color.fill;
-				ctx.StrokeStyle = this.color.stroke;
+				if(this.image)
+				{
+				    // no need to set styles
+				}
+				
+				else
+				{
+				    ctx.fillStyle   = this.color.fill;
+				    ctx.StrokeStyle = this.color.stroke;
+				}
 				
 				break;
 				
 			default:
-				ctx.fillStyle   = this.color.fill;
-				ctx.strokeStyle = this.color.stroke;
+				if(this.image)
+				{
+				    // no need to set styles
+				}
+				
+				else
+				{
+				    ctx.fillStyle   = this.color.fill;
+				    ctx.strokeStyle = this.color.stroke;
+				}
 				
 				break;
 			}
 			
-			// draw
-			ctx.fillRect(this.center.x - this.size.width/2, this.center.y - this.size.height/2, this.size.width, this.size.height);
-			ctx.strokeRect(this.center.x - this.size.width/2, this.center.y - this.size.height/2, this.size.width, this.size.height);
-		
-		// revert changes - post rectangle
-		ctx.restore();
-		
-		// restore point - pre text
-		ctx.save();
-		
-			// text settings
-			//ctx.font = "12px Arial";
-			ctx.font      = this.text.size + "px " + this.text.font;
-			ctx.fillStyle = this.text.color;
-			ctx.textAlign = this.text.alignment;
+			// image drawing
+			if(this.image)
+			{
+			    ctx.drawImage(this.image, this.center.x - this.size.width/2,
+				    this.center.y - this.size.height/2, this.size.width, this.size.height);
+			}
 			
-			// draw text centered on the button
-			// TODO: Base drawing on size of text relative to size of button!
-			ctx.fillText(this.text.string, this.center.x, this.center.y + this.size.height/8);
+			// standard color / text drawing
+			else
+			{
+				// background
+			    ctx.fillRect(this.center.x - this.size.width/2, 
+			        this.center.y - this.size.height/2, this.size.width, this.size.height);
+			    ctx.strokeRect(this.center.x - this.size.width/2,
+			        this.center.y - this.size.height/2, this.size.width, this.size.height);
+					
+				// text settings
+			    ctx.font      = this.text.size + "px " + this.text.font;
+			    ctx.fillStyle = this.text.color;
+			    ctx.textAlign = this.text.alignment;
+			
+			    // draw text centered on the button
+			    // TODO: Base drawing on size of text relative to size of button!
+			    ctx.fillText(this.text.string, this.center.x, this.center.y + this.size.height/8);
+			}
 		
 		// revert changes - post text
 		ctx.restore();
